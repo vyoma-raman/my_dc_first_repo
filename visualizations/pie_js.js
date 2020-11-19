ec7496fc4fd05f255f1f85de0af93b09538f4b07
Vyoma Raman <vyoma.raman@berkeley.edu>
2020-11-19 11:01:12 -0800

    Fixed pie tooltip and added bar tooltip

--- visualizations/pie_js.js
+++ visualizations/pie_js.js
@@ -23,60 +23,52 @@ select
   })
 
 const svg2 = d3
-  .select('#chart')
-  .append('svg')
-  .attr('width', width + margin.left + margin.right)
-  .attr('height', height + margin.top + margin.bottom)
-
-const g2 = svg2
-  .append('g')
-  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+  .select('#my_dataviz2')
+.append("svg")
+    .attr("width", width)
+    .attr("height", height)
+.append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 const tooltip2 = d3.select("#chart").append("div")
   .attr("class", "tooltip");
 
 update()
 
+function update(d, i) {
+  let data2 = data[selectedCategory]
+  let pied_data2 = pie(d3.entries(data2))
+  svg2
+      .selectAll('all')
+      .data(pied_data2)
+      .enter()
+      .append('path')
+      .attr('d', d3.arc()
+          .innerRadius(radius * 0.5)
+          .outerRadius(radius)
+      )
+      .attr('fill', function(d){ return(color(d.data.key)) })
+      .attr("stroke", "black")
+      .style("stroke-width", "1px")
+      .on("mouseover", function(d, i) {
+          d3.select(this)
+              .style("opacity", 0.7)
+          tooltip2.transition()
+              .style("opacity", 1)
+          tooltip2.html("<strong>"+ keyDict[keys[i]] + "</strong><br>" + Math.round(data2[keys[i]] / total(data2) * 10000) / 100 + "%")
+              .style("left", (d3.event.pageX + 10) + "px")
+              .style("top", (d3.event.pageY - 15) + "px")
+        })
+      .on("mouseout", function(d) {
+          d3.select(this)
+              .style("opacity", 1)
+          tooltip2.transition()
+              .style("opacity", 0)
+          })
+};
+
 select.on("change", function (d) {
       selectedCategory = d3.select(this).property("value");
-      update()
+      svg2.selectAll("*").remove();
+      update();
   })
-
-function update(d, i) {
-  let ls = data[selectedCategory]
-  let values = Object.keys(ls).map(function(key){
-    return ls[key];
-  });
-  let pie2 = d3.pie();
-  let arc2 = d3.arc()
-    .innerRadius(radius * 0.5)
-    .outerRadius(radius);
-  let arcs2 = g2.selectAll("arc")
-    .data(pie(values))
-    .enter()
-    .append("g")
-    .attr("class", "arc")
-  arcs2.append("path")
-    .attr("fill", function(d, i) {
-        return color(i);
-    })
-    .attr("d", arc2)
-    .on("mouseover", function(d, i) {
-        d3.select(this)
-          .style("opacity", 0.7)
-        tooltip2.transition()
-          .style("opacity", 1)
-        tooltip2.html("<strong>Hello</strong>") // keyDict[keys[i]]
-          .style("left", (d3.event.pageX + 10) + "px")
-          .style("top", (d3.event.pageY - 15) + "px")
-          .style("background-color", "#FF0000")
-          .style("color", "#FF0000")
-        console.log(keyDict[keys[i]])
-      })
-    .on("mouseout", function(d) {
-      d3.select(this)
-        .style("opacity", 1)
-      tooltip2.transition()
-        .style("opacity", 0)
-      })
-};
